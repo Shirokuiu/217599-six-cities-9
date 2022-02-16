301e650735cb4794429ba17646305ac5927eea27
pavel <shirokuiuna@mail.ru>
2022-02-16 12:04:45 +0300

    Добавил слеш, так как при редиректе нужно возвращать на /login и тп. Также добавил в пропсы authorizationStatus

--- project/src/types/app.ts
+++ project/src/types/app.ts
@@ -1,11 +1,14 @@
+import { AuthorizationStatus } from 'src/types/auth';
+
 export type AppProps = {
   placesFound: number;
+  authorizationStatus: AuthorizationStatus;
 };
 
 export enum AppRoutingPath {
   Index = '/',
   NotFound = '*',
-  Login = 'login',
-  Favorites = 'favorites',
-  Offer = 'offer/:id',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id',
 }

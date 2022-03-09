adfd9258c2ff28f07b242591a2e1a1db8c65df07
pavel <shirokuiuna@mail.ru>
2022-03-09 21:15:03 +0300

    Создал сервис для работы с токеном авторизации

--- project/src/services/token.ts
+++ project/src/services/token.ts
@@ -1,8 +1,17 @@
 import { TOKEN_KEY_LOCAL_STORAGE } from 'src/services/constants/constants';
+import { TokenItem } from 'src/types/token';
 
 class Token {
-  static get(): Token | undefined {
-    return localStorage.getItem(TOKEN_KEY_LOCAL_STORAGE) as Token;
+  static get(): TokenItem | null {
+    return localStorage.getItem(TOKEN_KEY_LOCAL_STORAGE);
+  }
+
+  static set(token: TokenItem) {
+    localStorage.setItem(TOKEN_KEY_LOCAL_STORAGE, token);
+  }
+
+  static remove() {
+    localStorage.removeItem(TOKEN_KEY_LOCAL_STORAGE);
   }
 }
 

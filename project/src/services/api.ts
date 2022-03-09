33c94e85121a4fbc66b02887045159069d836fff
pavel <shirokuiuna@mail.ru>
2022-03-09 21:40:54 +0300

    Добавил конфиг api для поддержки токена авторизации

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,9 +1,23 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 import { BACKEND_URL, REQUEST_TIMEOUT } from 'src/services/constants/constants';
+import Token from 'src/services/token';
 
-export const createAPI = (): AxiosInstance =>
-  axios.create({
+export const createAPI = (): AxiosInstance => {
+  const api = axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
   });
+
+  api.interceptors.request.use((config: AxiosRequestConfig) => {
+    const token: Token | null = Token.get();
+
+    if (token) {
+      config.headers['x-token'] = token;
+    }
+
+    return config;
+  });
+
+  return api;
+};

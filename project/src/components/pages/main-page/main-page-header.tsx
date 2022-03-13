ba882078ad3bcb53539e192ed703cc09c9416c63
Shirokuiu <shirokuiuna@mail.ru>
2022-03-13 20:13:50 +0300

    Вынес логотип в отдельный компонент

--- project/src/components/pages/main-page/main-page-header.tsx
+++ project/src/components/pages/main-page/main-page-header.tsx
@@ -1,4 +1,5 @@
 import MainPageUserBlock from 'src/components/pages/main-page/main-page-user-block';
+import LogoLink from 'src/components/shared/logo-link/logo-link';
 
 function MainPageHeader() {
   return (
@@ -6,15 +7,7 @@ function MainPageHeader() {
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <a className="header__logo-link header__logo-link--active">
-              <img
-                className="header__logo"
-                src="img/logo.svg"
-                alt="6 cities logo"
-                width={81}
-                height={41}
-              />
-            </a>
+            <LogoLink isActive />
           </div>
           <nav className="header__nav">
             <MainPageUserBlock />

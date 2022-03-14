d76facd0e3e1cb2cf9e2ef499f2bdaaf20abc56e
Shirokuiu <shirokuiuna@mail.ru>
2022-03-14 21:16:49 +0300

    Вывел почту залогиненного пользователя

--- project/src/components/pages/main-page/main-page-user-block.tsx
+++ project/src/components/pages/main-page/main-page-user-block.tsx
@@ -2,13 +2,12 @@ import { useAppSelector } from 'src/hooks';
 import { AuthorizationStatus } from 'src/types/auth';
 import UserLoggedIn from 'src/components/shared/user-logged-in/user-logged-in';
 import UserLoggedOut from 'src/components/shared/user-logged-out/user-logged-out';
+import { User } from 'src/types/user';
 
-const switchAuthUserComponent = (
-  authStatus: AuthorizationStatus,
-): JSX.Element => {
+const switchAuthUserComponent = (authStatus: AuthorizationStatus, me?: User): JSX.Element => {
   switch (authStatus) {
     case AuthorizationStatus.Auth:
-      return <UserLoggedIn />;
+      return <UserLoggedIn user={me} />;
     case AuthorizationStatus.NoAuth:
       return <UserLoggedOut />;
     default:
@@ -17,13 +16,9 @@ const switchAuthUserComponent = (
 };
 
 function MainPageUserBlock() {
-  const { authorizationStatus } = useAppSelector((state) => state);
+  const { authorizationStatus, me } = useAppSelector((state) => state);
 
-  return (
-    <ul className="header__nav-list">
-      {switchAuthUserComponent(authorizationStatus)}
-    </ul>
-  );
+  return <ul className="header__nav-list">{switchAuthUserComponent(authorizationStatus, me)}</ul>;
 }
 
 export default MainPageUserBlock;
--- project/src/components/shared/user-logged-in/user-logged-in.tsx
+++ project/src/components/shared/user-logged-in/user-logged-in.tsx
@@ -1,19 +1,15 @@
 import { Link } from 'react-router-dom';
 
 import { AppRoutingPath } from 'src/types/app';
+import { UserLoggedInProps } from 'src/types/user-logged-in';
 
-function UserLoggedIn() {
+function UserLoggedIn({ user }: UserLoggedInProps) {
   return (
     <>
       <li className="header__nav-item user">
-        <Link
-          className="header__nav-link header__nav-link--profile"
-          to={AppRoutingPath.Favorites}
-        >
+        <Link className="header__nav-link header__nav-link--profile" to={AppRoutingPath.Favorites}>
           <div className="header__avatar-wrapper user__avatar-wrapper" />
-          <span className="header__user-name user__name">
-            Oliver.conner@gmail.com
-          </span>
+          <span className="header__user-name user__name">{user?.email}</span>
         </Link>
       </li>
       <li className="header__nav-item">

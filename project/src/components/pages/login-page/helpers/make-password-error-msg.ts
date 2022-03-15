1eb85717aae87c3b7f45b35a536e7834ac78a8d3
Shirokuiu <shirokuiuna@mail.ru>
2022-03-15 19:54:31 +0300

    Отрефакторил

--- project/src/components/pages/login-page/helpers/make-password-error-msg.ts
+++ project/src/components/pages/login-page/helpers/make-password-error-msg.ts
@@ -1,21 +1,15 @@
 import { ValidationPattern } from 'src/components/pages/login-page/constants/constants';
 
 export const makePasswordErrorMsg = (value: string): string => {
-  let messages: string[] = [];
+  const messages = new Set<string>([]);
 
   if (!ValidationPattern.AT_LEAST_ONE_DIGIT.test(value)) {
-    messages.push('Введите хотя бы одно число');
-
-    // NOTE Очищаю дубликаты
-    messages = [...new Set(messages)];
+    messages.add('Введите хотя бы одно число');
   }
 
   if (!ValidationPattern.AT_LEAST_ONE_ENGLISH_LETTER.test(value)) {
-    messages.push('Введите хотя бы одну латинскую букву');
-
-    // NOTE Очищаю дубликаты
-    messages = [...new Set(messages)];
+    messages.add('Введите хотя бы одну латинскую букву');
   }
 
-  return messages.join('. ');
+  return [...messages].join('. ');
 };

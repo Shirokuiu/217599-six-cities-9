a27b8cb7b9f4685652464f5400aa5cb299e7288c
Shirokuiu <shirokuiuna@mail.ru>
2022-03-20 12:10:52 +0300

    убрал оптимизацию, потому что глючит )

--- project/src/components/pages/main-page/main-page-location-tabs.tsx
+++ project/src/components/pages/main-page/main-page-location-tabs.tsx
@@ -15,10 +15,7 @@ const locationTabs: Tab[] = buildLocationTabs();
 function MainPageLocationTabs() {
   // NOTE Уточнить, как оптимизировать searchParams, который перерендеривает компонент на любое изменение строки, даже из другого компонента
   const [searchParams, setSearchParams] = useSearchParams({});
-  const groupedCities = useAppSelector(
-    (state) => state.MAIN_PAGE.groupedCities,
-    (prev, next) => prev !== next,
-  );
+  const groupedCities = useAppSelector((state) => state.MAIN_PAGE.groupedCities);
   const dispatch = useAppDispatch();
   const parsedSearchParams = parseSearchParams<LocationTabSearchParam>(searchParams);
 

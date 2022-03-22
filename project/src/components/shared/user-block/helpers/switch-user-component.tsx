2d1608f0bea34c30a83ddaf4779c176ea7e10437
Shirokuiu <shirokuiuna@mail.ru>
2022-03-22 19:09:49 +0300

    Вынес в отдельнй компонент UserBlock

---
+++ project/src/components/shared/user-block/helpers/switch-user-component.tsx
@@ -0,0 +1,19 @@
+import { AuthorizationStatus } from 'src/types/auth';
+import { User } from 'src/types/user';
+import UserLoggedIn from 'src/components/shared/user-logged-in/user-logged-in';
+import UserLoggedOut from 'src/components/shared/user-logged-out/user-logged-out';
+
+export const switchUserComponent = (
+  authStatus: AuthorizationStatus,
+  cb: () => void,
+  me?: User,
+): JSX.Element => {
+  switch (authStatus) {
+    case AuthorizationStatus.Auth:
+      return <UserLoggedIn user={me} onLogoutClick={cb} />;
+    case AuthorizationStatus.NoAuth:
+      return <UserLoggedOut />;
+    default:
+      return <UserLoggedOut />;
+  }
+};
---
+++ project/src/types/user-block.ts
@@ -0,0 +1,8 @@
+import { AuthorizationStatus } from 'src/types/auth';
+import { User } from 'src/types/user';
+
+export type UserBlockProps = {
+  authorizationStatus: AuthorizationStatus;
+  me?: User;
+  onLogoutClick?: () => void;
+};

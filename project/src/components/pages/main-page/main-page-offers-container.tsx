e2083b787f0487b9c291aca2011674a1e6e4a42a
Shirokuiu <shirokuiuna@mail.ru>
2022-03-25 00:17:15 +0300

    Вынус функцию в хелпер

--- project/src/components/pages/main-page/main-page-offers-container.tsx
+++ project/src/components/pages/main-page/main-page-offers-container.tsx
@@ -1,16 +1,10 @@
 import { useAppSelector } from 'src/hooks';
-import MainPageOffersEmpty from 'src/components/pages/main-page/main-page-offers-empty';
-import { Offer } from 'src/types/offer';
-
-import MainPageOffers from 'src/components/pages/main-page/main-page-offers';
-
-const switchComponents = (offers?: Offer[]): JSX.Element =>
-  offers?.length ? <MainPageOffers /> : <MainPageOffersEmpty />;
+import { switchMainPageComponents } from 'src/components/pages/main-page/helpers/switch-main-page-components';
 
 export function MainPageOffersContainer() {
   const currentCity = useAppSelector((state) => state.MAIN_PAGE.currentCity);
 
-  return switchComponents(currentCity?.offers);
+  return switchMainPageComponents(currentCity?.offers);
 }
 
 export default MainPageOffersContainer;

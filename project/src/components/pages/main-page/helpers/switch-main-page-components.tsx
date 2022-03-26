d667a54fa1c92ca6a497357f1d132c4accebb629
Shirokuiu <shirokuiuna@mail.ru>
2022-03-26 18:45:26 +0300

    Свитч компонента главной страницы

---
+++ project/src/components/pages/main-page/helpers/switch-main-page-components.tsx
@@ -0,0 +1,6 @@
+import { Offer } from 'src/types/offer';
+import MainPageOffers from 'src/components/pages/main-page/main-page-offers';
+import MainPageOffersEmpty from 'src/components/pages/main-page/main-page-offers-empty';
+
+export const switchMainPageComponents = (offers?: Offer[]): JSX.Element =>
+  offers?.length ? <MainPageOffers /> : <MainPageOffersEmpty />;

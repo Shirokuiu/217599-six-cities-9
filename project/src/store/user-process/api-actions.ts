0734970b9515b2f4638de5c48f0e6208da8b0cf9
Shirokuiu <shirokuiuna@mail.ru>
2022-03-28 11:40:59 +0300

    Вызываю dispatch из пропсов thunk, а не импортируя из стора

--- project/src/store/user-process/api-actions.ts
+++ project/src/store/user-process/api-actions.ts
@@ -1,6 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-import { store } from 'src/store/index';
 import { AuthorizationStatus } from 'src/types/auth';
 import { LoginPageFormBody } from 'src/types/login-page';
 import Token from 'src/services/token';
@@ -8,23 +7,28 @@ import { setAuthStatus, setMe } from 'src/store/user-process/user-process';
 import UserService from 'src/services/user-service/user-service';
 import { ActionType } from 'src/store/user-process/action-type';
 
-export const checkAuth = createAsyncThunk(ActionType.CheckAuth, async () => {
+export const checkAuth = createAsyncThunk(ActionType.CheckAuth, async (_arg, { dispatch }) => {
   const data = await UserService.checkAuth();
 
-  store.dispatch(setAuthStatus(AuthorizationStatus.Auth));
-  store.dispatch(setMe(data));
+  dispatch(setAuthStatus(AuthorizationStatus.Auth));
+  dispatch(setMe(data));
 });
-export const login = createAsyncThunk(ActionType.Login, async (body: LoginPageFormBody) => {
-  const data = await UserService.login(body);
 
-  Token.set(data.token);
-  store.dispatch(setAuthStatus(AuthorizationStatus.Auth));
-  store.dispatch(setMe(data));
-});
-export const logout = createAsyncThunk(ActionType.Logout, async () => {
+export const login = createAsyncThunk(
+  ActionType.Login,
+  async (body: LoginPageFormBody, { dispatch }) => {
+    const data = await UserService.login(body);
+
+    Token.set(data.token);
+    dispatch(setAuthStatus(AuthorizationStatus.Auth));
+    dispatch(setMe(data));
+  },
+);
+
+export const logout = createAsyncThunk(ActionType.Logout, async (_arg, { dispatch }) => {
   await UserService.logout();
 
   Token.remove();
-  store.dispatch(setAuthStatus(AuthorizationStatus.NoAuth));
-  store.dispatch(setMe(undefined));
+  dispatch(setAuthStatus(AuthorizationStatus.NoAuth));
+  dispatch(setMe(undefined));
 });

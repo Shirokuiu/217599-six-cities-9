9b7f4191a48d64c809e2f4f785a3c34ca8b5fa86
Shirokuiu <shirokuiuna@mail.ru>
2022-03-28 16:51:30 +0300

    Написал тесты для простого компонента - Dropdown

--- project/src/components/shared/dropdown/dropdown.tsx
+++ project/src/components/shared/dropdown/dropdown.tsx
@@ -1,19 +1,13 @@
 import { DropdownItem, DropdownProps } from 'src/types/dropdown';
 
-function Dropdown({
-  items,
-  onDropdownItemClick = () => undefined,
-}: DropdownProps) {
+function Dropdown({ items, onDropdownItemClick = () => undefined }: DropdownProps) {
   return (
     <ul className="places__options places__options--custom places__options--opened">
       {items.map((dropdownItem: DropdownItem) => (
         <li
           key={dropdownItem.id}
           onClick={() => onDropdownItemClick(dropdownItem)}
-          className={[
-            'places__option',
-            dropdownItem.isActive && 'places__option--active',
-          ]
+          className={['places__option', dropdownItem.isActive && 'places__option--active']
             .filter(Boolean)
             .join(' ')}
           tabIndex={0}

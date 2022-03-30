8b31f5ac06f01594a515124339312953738bb743
pavel <shirokuiuna@mail.ru>
2022-03-30 15:51:20 +0300

    Написал тесты для простого компонента - FavoritePreview

---
+++ project/src/components/shared/favorite-preview/favorite-preview.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import { offers } from 'src/mocks/offers';
+import FavoritePreview from 'src/components/shared/favorite-preview/favorite-preview';
+
+describe('Проверка на корректность рендера компонента FavoritePreview', () => {
+  describe('Проверка передачи параметров', () => {
+    it('Компонент верно отрисовывается при переданных параметрах', async () => {
+      const { container } = render(
+        <BrowserRouter>
+          <FavoritePreview favorite={offers[0]} />
+        </BrowserRouter>,
+      );
+
+      expect(container).toBeInTheDocument();
+      expect(screen.getByTestId('place-image').getAttribute('src')).toBe(offers[0].previewImage);
+    });
+  });
+});

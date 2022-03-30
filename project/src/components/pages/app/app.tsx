11223999410d749be70f6f9966e9f19f65c7df03
pavel <shirokuiuna@mail.ru>
2022-03-30 17:02:34 +0300

    Перенес в папку, для удобства тестов

--- project/src/components/pages/app/app.tsx
+++ project/src/components/pages/app/app.tsx
@@ -1,4 +1,4 @@
-import AppRouting from 'src/components/pages/app/app-routing';
+import AppRouting from 'src/components/pages/app/app-routing/app-routing';
 import { store } from 'src/store';
 import { checkAuth } from 'src/store/user-process/api-actions/api-actions';
 import { useEffect } from 'react';

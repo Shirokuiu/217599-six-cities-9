5eb11a24c0d8d1435226620df97a3feb7cc7d0b9
pavel <shirokuiuna@mail.ru>
2022-03-30 15:24:46 +0300

    Написал тесты для простого компонента - UserLoggedOut

---
+++ project/src/components/shared/user-logged-out/user-logged-out.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import UserLoggedOut from 'src/components/shared/user-logged-out/user-logged-out';
+
+describe('Проверка на корректность рендера компонента UserLoggedOut', () => {
+  describe('Проверка передачи параметров', () => {
+    it('Компонент верно отрисовывается c переданными параметрами', async () => {
+      const { container } = render(
+        <BrowserRouter>
+          <UserLoggedOut />
+        </BrowserRouter>,
+      );
+
+      expect(container).toBeInTheDocument();
+      expect(screen.getByTestId('logout-signout')).toHaveTextContent('Sign in');
+    });
+  });
+});

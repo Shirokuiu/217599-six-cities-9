321087a772cb07b42284530a580cf6ef39d954b8
pavel <shirokuiuna@mail.ru>
2022-03-30 16:34:12 +0300

    id для теста

--- project/src/components/pages/not-found-page/not-found-page.tsx
+++ project/src/components/pages/not-found-page/not-found-page.tsx
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom';
 function NotFoundPage() {
   return (
     <>
-      <h1>Page not found</h1>
-      <Link to="/">Go to main page</Link>
+      <h1 data-testid="title">Page not found</h1>
+      <Link to="/" data-testid="link">
+        Go to main page
+      </Link>
     </>
   );
 }

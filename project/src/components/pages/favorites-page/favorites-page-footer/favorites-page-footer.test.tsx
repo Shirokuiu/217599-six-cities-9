3bde688c67acd77477fdddedf7e1db65401940b2
pavel <shirokuiuna@mail.ru>
2022-03-30 16:22:46 +0300

    Написал тесты для простого компонента - FavoritesPageFooter

---
+++ project/src/components/pages/favorites-page/favorites-page-footer/favorites-page-footer.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import FavoritesPageFooter from 'src/components/pages/favorites-page/favorites-page-footer/favorites-page-footer';
+import { AppRoutingPath } from 'src/types/app';
+
+describe('Проверка на корректность рендера компонента FavoritesPageFooter', () => {
+  describe('Проверка передачи параметров', () => {
+    it('Компонент верно отрисовывается', async () => {
+      const { container } = render(
+        <BrowserRouter>
+          <FavoritesPageFooter />
+        </BrowserRouter>,
+      );
+
+      expect(container).toBeInTheDocument();
+      expect(screen.getByTestId('logo-link').getAttribute('href')).toBe(AppRoutingPath.Index);
+    });
+  });
+});

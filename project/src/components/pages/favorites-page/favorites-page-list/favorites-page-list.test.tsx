2eb274e55d4116fe097cafcba3939b600f5955b8
pavel <shirokuiuna@mail.ru>
2022-03-31 22:00:14 +0300

    Написал тест для компонента - FavoritesPageList

---
+++ project/src/components/pages/favorites-page/favorites-page-list/favorites-page-list.test.tsx
@@ -0,0 +1,28 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { Provider } from 'react-redux';
+
+import FavoritesPageList from 'src/components/pages/favorites-page/favorites-page-list/favorites-page-list';
+import { groupCities } from 'src/helpers/group-cities';
+import { offers } from 'src/mocks/offers';
+
+describe('Проверка на корректность рендера компонента FavoritesPageList', () => {
+  describe('Проверка передачи параметров', () => {
+    it('Компонент верно отрисовывается', () => {
+      const groupedFavorites = groupCities(offers);
+      const mockStore = configureMockStore();
+
+      const { container } = render(
+        <Provider store={mockStore()}>
+          <FavoritesPageList favorites={groupedFavorites} />
+        </Provider>,
+        {
+          wrapper: BrowserRouter,
+        },
+      );
+
+      expect(container).toBeInTheDocument();
+    });
+  });
+});

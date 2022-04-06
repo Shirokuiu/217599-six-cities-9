aba2410d41574367065cb5b943a5abc42f069d2f
pavel <shirokuiuna@mail.ru>
2022-04-06 15:33:11 +0300

    Дописал логику отображения Bookmark

--- project/src/components/shared/bookmark/bookmark/bookmark.tsx
+++ project/src/components/shared/bookmark/bookmark/bookmark.tsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
 
-import { BookmarkProps } from 'src/types/bookmark';
+import { BookmarkProps, StyleMode } from 'src/types/bookmark';
 import { getAriaText } from 'src/components/shared/bookmark/helpers/get-aria-text';
 import { toggleActiveBookmarkClass } from 'src/components/shared/bookmark/helpers/toogle-active-bookmark-class';
+import BookmarkIcon from 'src/components/shared/bookmark/bookmark-icon';
 
-function Bookmark({ isActive: inBookmark, onToggleActive = () => undefined }: BookmarkProps) {
+function Bookmark({
+  isActive: inBookmark,
+  className = 'place-card__bookmark-button',
+  classNameIcon = 'place-card__bookmark-icon',
+  styleMode = StyleMode.Default,
+  onToggleActive = () => undefined,
+}: BookmarkProps) {
   const [isActive, setIsActive] = useState(inBookmark);
 
   const handleBookmarkClick = () => {
@@ -18,13 +25,11 @@ function Bookmark({ isActive: inBookmark, onToggleActive = () => undefined }: Bo
   return (
     <button
       onClick={handleBookmarkClick}
-      className={toggleActiveBookmarkClass({ isActive })}
+      className={toggleActiveBookmarkClass({ isActive, className })}
       type="button"
       data-testid="bookmark"
     >
-      <svg className="place-card__bookmark-icon" width={18} height={19}>
-        <use xlinkHref="#icon-bookmark" />
-      </svg>
+      <BookmarkIcon styleMode={styleMode} classNameIcon={classNameIcon} />
       <span className="visually-hidden" data-testid="aria-text">
         {getAriaText(isActive)}
       </span>

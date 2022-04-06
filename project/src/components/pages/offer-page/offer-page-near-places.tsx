339ec2e848c923cc15d37b8f33a3c552bcf515fc
pavel <shirokuiuna@mail.ru>
2022-04-06 19:44:16 +0300

    WIP Вывел близлежащие предложения

---
+++ project/src/components/pages/offer-page/offer-page-near-places.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { useAppSelector } from 'src/hooks';
+import OfferPreview from 'src/components/shared/offer-preview/offer-preview';
+import Bookmark from 'src/components/shared/bookmark/bookmark/bookmark';
+
+function OfferPageNearPlaces() {
+  const nearOffers = useAppSelector((state) => state.OFFER_PAGE.nearOffers);
+
+  const handleImgMouseEnter = () => {
+    console.log('handleImgMouseEnter');
+  };
+
+  const handleImgMouseLeave = () => {
+    console.log('handleImgMouseLeave');
+  };
+
+  const handleToggleBookmark = (isActive: boolean, offerId: number) => {
+    console.log('handleToggleBookmark');
+  };
+
+  return (
+    <div className="container">
+      <section className="near-places places">
+        <h2 className="near-places__title">Other places in the neighbourhood</h2>
+        <div className="near-places__list places__list">
+          {nearOffers.map((offer) => (
+            <OfferPreview
+              key={offer.id}
+              offer={offer}
+              onImgMouseEnter={handleImgMouseEnter}
+              onImgMouseLeave={handleImgMouseLeave}
+              renderBookmark={() => (
+                <Bookmark
+                  isActive={offer.isFavorite}
+                  onToggleActive={(isActive) => handleToggleBookmark(isActive, offer.id)}
+                />
+              )}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default OfferPageNearPlaces;

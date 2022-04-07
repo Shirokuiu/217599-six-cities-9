0dbdc29131975dd5bad319a6ef7e89b5bcf4f532
pavel <shirokuiuna@mail.ru>
2022-04-07 12:47:19 +0300

    поправил описание тестов

--- project/src/components/shared/bookmark/bookmark-icon/bookmark-icon.test.tsx
+++ project/src/components/shared/bookmark/bookmark-icon/bookmark-icon.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import BookmarkIcon from 'src/components/shared/bookmark/bookmark-icon/bookmark-icon';
 import { StyleMode } from 'src/types/bookmark';
 
-describe('Проверка на корректность рендера компонента Bookmark', () => {
+describe('Проверка на корректность рендера компонента BookmarkIcon', () => {
   describe('Проверка передачи рендера', () => {
     it('Компонент верно отрисоывается', () => {
       const { container } = render(
--- project/src/components/shared/review-form/review-form.test.tsx
+++ project/src/components/shared/review-form/review-form.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 
 import ReviewForm from 'src/components/shared/review-form/review-form';
 
-describe('Проверка на корректность рендера компонента Loader', () => {
+describe('Проверка на корректность рендера компонента ReviewForm', () => {
   describe('Проверка рендера', () => {
     it('Компонент верно отрисовывается', () => {
       const { container } = render(<ReviewForm />);

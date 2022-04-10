f3dd7d250503641ec31862d1343ecf64b0f08799
Shirokuiu <shirokuiuna@mail.ru>
2022-04-10 11:05:54 +0300

    Заменил иконку маркера на верный по ТЗ + поменял его размеры + поменял иконку активного маркера

--- project/src/components/shared/map-l/constants/constants.ts
+++ project/src/components/shared/map-l/constants/constants.ts
@@ -2,21 +2,18 @@ import { MapLZoom, MapLConfig } from 'src/types/map-l';
 
 export const MAP_L_CONFIG: MapLConfig = {
   DEFAULT_CUSTOM_ICON: {
-    iconUrl:
-      'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
+    iconUrl: 'img/pin.svg',
+    iconSize: [27, 39],
+    iconAnchor: [27, 39],
   },
   CURRENT_CUSTOM_ICON: {
-    iconUrl:
-      'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
+    iconUrl: 'img/pin-active.svg',
+    iconSize: [27, 39],
+    iconAnchor: [27, 39],
   },
   ZOOM: MapLZoom.DefaultZoom,
   TITLE_LAYER: {
-    urlTemplate:
-      'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+    urlTemplate: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
     attribution:
       '\'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>\'',
   },
